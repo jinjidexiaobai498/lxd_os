194cee88b8abef3906c7d1c0353178ef131a4f43
jinjidexiaobai498 <2511826380@qq.com>
2023-03-02 12:58:03 +0800

    a minimal rust kernel

--- configure.sh
+++ configure.sh
@@ -1,5 +1,5 @@
 
-rustup override set nightly
+# freestanding rust bin
 rustup target add thumbv7em-none-eabihf
 cargo build --target thumbv7em-none-eabihf
 # Linux
@@ -8,3 +8,11 @@ cargo rustc -- -C link-arg=-nostartfiles
 cargo rustc -- -C link-args="/ENTRY:_start /SUBSYSTEM:console"
 # macOS
 cargo rustc -- -C link-args="-e __start -static -nostartfiles"
+
+# a minimal rust kernel
+rustup override set nightly
+rustc --version --verbaose
+cargo build --target x86_64-lxd_os.json
+rustup component add rust-src
+cargo install bootimage
+rustup component add llvm-tools-preview
